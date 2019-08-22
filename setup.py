e525c1d905b412885d7408c9a0038311c3aa57ac
Ruben Di Battista <ruben.di-battista@polytechnique.edu>
2019-08-22 14:55:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,59 +1,125 @@
-from setuptools import setup
-# To use a consistent encoding
-from codecs import open
-from os import path
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Get version in namespace
-with open('ovh_dynhost/_version.py') as f:
-    code = compile(f.read(), "_version.py", 'exec')
-    exec(code)
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
 
-here = path.abspath(path.dirname(__file__))
+import io
+import os
+import sys
+from shutil import rmtree
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import find_packages, setup, Command
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+# Package meta-data.
+NAME = "ovh-dynhost"
+DESCRIPTION = "A script to update the OVH DynHost with your current public IP',
+URL = "https://github.com/rubendibattista/ovh-dynhost"
+EMAIL = "rubendibattista@gmail.com"
+AUTHOR = "Ruben Di Battista"
+REQUIRES_PYTHON = ">=3.5.0"
+VERSION = None
 
+# What packages are required for this module to be executed?
+REQUIRED = ["requests", "docopt"]
+
+# What packages are optional?
+EXTRAS = {}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for
+# that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='ovh-dynhost',
-    version=__version__,
-    description='A script to update the OVH DynHost service public IP',
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
     long_description=long_description,
-
-    url='https://github.com/rubendibattista/ovh-dynhost',
-
-    license='BSD',
-
-    author='Ruben Di Battista',
-
-    author_email='rubendibattista@gmail.com',
-
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]
+    ),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+    entry_points={"console_scripts": ["ovh-dynhost=ovh_dynhost:main"]},
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="BSD",
     classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: System Administrators',
-        'Topic :: Internet :: Name Service (DNS)',
-
         'Environment :: Console',
-
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
-
         'Programming Language :: Python :: 3',
+        'Topic :: Internet :: Name Service (DNS)',
     ],
-
-    keywords='ovh dynhost dynamic dns',
-    packages=['ovh_dynhost'],
-    install_requires=['requests', 'docopt'],
-    extras_require={
-        'dev': ['twine']
-    },
-
-    entry_points={  # Optional
-        'console_scripts': [
-            'ovh-dynhost=ovh_dynhost:main',
-        ],
-    },
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
 )
