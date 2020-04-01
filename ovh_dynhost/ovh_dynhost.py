bd51942c9095d296bcdd6387dcd76c1134614993
Ruben Di Battista <rubendibattista@gmail.com>
2020-04-01 16:57:46 +0200

    Fix __version__ import

--- ovh_dynhost/ovh_dynhost.py
+++ ovh_dynhost/ovh_dynhost.py
@@ -47,36 +47,36 @@ import sys
 import requests
 import docopt
 
-from ._version import __version__
+from .__version__ import __version__
 
-DEFAULT_PUBLIC_IP_API_URL = 'https://api.ipify.org'
-DEFAULT_CONF_PATH = os.path.join(os.getenv('HOME'), '.ovh-dynhost.conf')
-OVH_API_ENDPOINT = 'https://www.ovh.com/nic/update'
+DEFAULT_PUBLIC_IP_API_URL = "https://api.ipify.org"
+DEFAULT_CONF_PATH = os.path.join(os.getenv("HOME"), ".ovh-dynhost.conf")
+OVH_API_ENDPOINT = "https://www.ovh.com/nic/update"
 
 # EXIT CODES
 SAME_IP_ERROR = 75
 GENERAL_ERROR = 1
 
 
-def get_conf(conf_path, hostname=None, username=None,
-             password=None):
+def get_conf(conf_path, hostname=None, username=None, password=None):
     global LOGGER
 
     if not os.path.exists(conf_path):
-        LOGGER.warning("No config file found at {conf_path}".format(
-            conf_path=conf_path))
+        LOGGER.warning(
+            "No config file found at {conf_path}".format(conf_path=conf_path)
+        )
         return (hostname, username, password)
 
-    conf_file = open(conf_path, 'r')
+    conf_file = open(conf_path, "r")
     conf = json.load(conf_file)
     conf_file.close()
 
     if hostname is None:
-        hostname = conf['hostname']
+        hostname = conf["hostname"]
     if username is None:
-        username = conf['username']
+        username = conf["username"]
     if password is None:
-        password = conf['password']
+        password = conf["password"]
 
     return (hostname, username, password)
 
@@ -85,22 +85,21 @@ def main():
     global LOGGER
 
     # Parse arguments
-    args = docopt.docopt(
-        __doc__, version=__version__)
+    args = docopt.docopt(__doc__, version=__version__)
 
-    hostname = args['<hostname>']
-    username = args['<username>']
-    password = args['<password>']
-    public_ip_api_url = args['--pub-ip-source']
-    logfile = args['--log-file']
-    conf_file = args['--conf-file']
-    debug = args['--debug']
+    hostname = args["<hostname>"]
+    username = args["<username>"]
+    password = args["<password>"]
+    public_ip_api_url = args["--pub-ip-source"]
+    logfile = args["--log-file"]
+    conf_file = args["--conf-file"]
+    debug = args["--debug"]
 
-    if not(conf_file):
+    if not (conf_file):
         conf_file = DEFAULT_CONF_PATH
 
     # Logging Configuration
-    LOGGER = logging.getLogger('ovh-dynhost')
+    LOGGER = logging.getLogger("ovh-dynhost")
     logging_level = logging.INFO
     if debug:
         logging_level = logging.DEBUG
@@ -116,18 +115,23 @@ def main():
         logging.basicConfig(
             filename=logfile,
             level=logging_level,
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        )
         LOGGER.info("Logging to {logfile}".format(logfile=logfile))
 
-    (hostname, username, password) = get_conf(conf_path=conf_file,
-                                              hostname=hostname,
-                                              username=username,
-                                              password=password)
+    (hostname, username, password) = get_conf(
+        conf_path=conf_file,
+        hostname=hostname,
+        username=username,
+        password=password,
+    )
 
-    LOGGER.debug('Hostname {hostname} username {username} '
-                 'password {password}'.format(hostname=hostname,
-                                              username=username,
-                                              password=password))
+    LOGGER.debug(
+        "Hostname {hostname} username {username} "
+        "password {password}".format(
+            hostname=hostname, username=username, password=password
+        )
+    )
 
     if public_ip_api_url:
         url = public_ip_api_url
@@ -136,8 +140,8 @@ def main():
 
     LOGGER.debug("Retrieving Public IP from:{}".format(url))
 
-    if args['--ip']:
-        ip = args['--ip']
+    if args["--ip"]:
+        ip = args["--ip"]
     else:
         # Retrieving public IP from web service
         ip = requests.get(url).text
@@ -145,21 +149,20 @@ def main():
     LOGGER.info("Public IP: {}".format(ip))
 
     # Building Request
-    payload = {
-        'myip': ip,
-        'system': 'dyndns',
-        'hostname': hostname
-    }
+    payload = {"myip": ip, "system": "dyndns", "hostname": hostname}
 
-    headers = {
-        'user-agent': 'ovh-dynhost/' + __version__
-    }
+    headers = {"user-agent": "ovh-dynhost/" + __version__}
 
     authentication = requests.auth.HTTPBasicAuth(username, password)
 
     # Run Request
-    request = requests.Request('GET', url=OVH_API_ENDPOINT, params=payload,
-                               headers=headers, auth=authentication).prepare()
+    request = requests.Request(
+        "GET",
+        url=OVH_API_ENDPOINT,
+        params=payload,
+        headers=headers,
+        auth=authentication,
+    ).prepare()
 
     session = requests.Session()
     response = session.send(request).text.lower()
@@ -172,10 +175,12 @@ def main():
         sys.exit(SAME_IP_ERROR)
     else:
         LOGGER.error(
-            "Error occurred in updating IP. Response from server:{}"
-            .format(response))
+            "Error occurred in updating IP. Response from server:{}".format(
+                response
+            )
+        )
         sys.exit(GENERAL_ERROR)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
